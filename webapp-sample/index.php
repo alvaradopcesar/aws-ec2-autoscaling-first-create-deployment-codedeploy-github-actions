638a2ef593d6f581cf3aa2ca7fe0e4187b7ad0e1
Cesar Alvarado <cesar.alvarado@thinknimble.com>
2024-11-16 21:50:35 -0500

    first change

---
+++ webapp-sample/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EC2 Public IP</title>
+</head>
+<body>
+    <h1>Welcome to my EC2 Instance</h1>
+    <p>Your EC2 instance's public IP is:
+        <?php echo $_SERVER['SERVER_ADDR']; ?>
+    </p>
+</body>
+</html>
